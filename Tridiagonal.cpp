1dda320788774f3e4655906fe14d2a37b4666869
adriabaa <adriabaa@student.matnat.uio.no>
2020-09-03 14:23:36 +0200

    Working on the general algorithm

--- Tridiagonal.cpp
+++ Tridiagonal.cpp
@@ -1,49 +1,72 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 
-//Closed-form solution
+//Writing to file
+ofstream ofile;
+
+// Analytical solution
 double closed_solution(double x){
-    return 1.0 - (1.0 - exp(-10))*x - exp(-10x);
+    return 1.0 - (1.0 - exp(-10))*x - exp(-10*x);
 }
 
 double source_term(double x){
-    return 100.0*exp(-10.0*x)
+    return 100.0*exp(-10.0*x);
 }
 
+void problemB(int n, string filename){
+  
 
-
-
-int main(){
-    
-    /* All vectors are at same length
-     * n+1 due to last and first being boundry
-     * 
-     */
 
 // Diagonal vectors 
     double *a_vector = new double[n + 1];
     double *b_vector = new double[n + 1];
     double *c_vector = new double[n + 1];
 
-    for(int i = 0; i < n; i++){
-        a_vector[i] = c_vector[i] = -1;
+// Numeric solution
+    double *v = new double[n + 1];
+
+// Solution matrix RHS
+    double *b_tilde = new double[n + 1];
+
+// Analytical solution
+    double *u = new double[n + 1];
+
+// Step size
+    double h = 1.0 / (n + 1.0);
+
+// Steps
+    double *x = new double[n+1];
+
+
+    for (int i = 0; i < n+1; i++){
+        b_tilde[i] = h*h*source_term(x[i]);
+        u[i] = closed_solution(x[i]);
+    }
+
+// Initializing the vectors
+    for (int i = 0; i <n+1; i++){
+        x[i] = i*h;
+        a_vector[i] = c_vector[i] = -1 ;
         b_vector[i] = 2;
     }
 
-// Step size
-    h = 1 / n
-    
+// General algorithm
+    for (int i = 1; i <n+1; i++){
+        b_vector[i] = b_vector[i] - a_vector[i] * c_vector[i-1]/b_vector[i-1];
+    }
+    cout << b_vector[7];
+
+}
 
 
 
 
-// General algorithm gauss
+int main(){
+    problemB(2, "test.txt");
 
-
-
-
-   return 0
+   return 0;
 }
 
